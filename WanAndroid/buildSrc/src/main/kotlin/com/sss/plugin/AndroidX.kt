9d47a9de4f6775ad030b917116422e51da1347f0
ZhangYiMing <MrZhangYiMing@163.com>
2021-11-16 22:29:14 +0800

    提交代码

---
+++ WanAndroid/buildSrc/src/main/kotlin/com/sss/plugin/AndroidX.kt
@@ -0,0 +1,14 @@
+package com.sss.plugin
+
+/**
+ * @author mrzhang
+ * @date 2021/11/16
+ */
+private object AndroidXVersion {
+    val appcompat = "1.2.0"
+}
+
+object AndroidX {
+    val appcompat = "androidx.appcompat:appcompat:${AndroidXVersion.appcompat}"
+}
+
