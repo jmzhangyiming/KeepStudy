103f3687b70c4e235c612669b6e43fd72a9b584d
zhangyiming <610872934@qq.com>
2021-11-17 17:26:09 +0800

    提交

---
+++ WanAndroid/app/src/main/java/com/study/wanandroid/SplashActivity.kt
@@ -0,0 +1,17 @@
+package com.study.wanandroid
+
+import android.os.Bundle
+import android.os.PersistableBundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
+
+class SplashActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+
+    }
+
+
+}
--- WanAndroid/buildSrc/src/main/kotlin/com/sss/plugin/AndroidX.kt
+++ WanAndroid/buildSrc/src/main/kotlin/com/sss/plugin/AndroidX.kt
@@ -6,9 +6,17 @@ package com.sss.plugin
  */
 private object AndroidXVersion {
     val appcompat = "1.2.0"
+    val core_version = "1.3.2"
+    val leak_canary = "1.5.4"
+    val constraintlayout = "2.0.4"
 }
 
 object AndroidX {
     val appcompat = "androidx.appcompat:appcompat:${AndroidXVersion.appcompat}"
+    val core_ktx = "androidx.core:core-ktx:${AndroidXVersion.core_version}"
+    val constraintlayout = "androidx.constraintlayout:constraintlayout:${AndroidXVersion.constraintlayout}"
+
+    val leak_canary_debug = "com.squareup.leakcanary:leakcanary-android:${AndroidXVersion.leak_canary}"
+    val leak_canary_release = "com.squareup.leakcanary:leakcanary-android-no-op:${AndroidXVersion.leak_canary}"
 }
 
