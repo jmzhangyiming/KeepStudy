6afc32b677fdefa1cab8e239f43722b42282730a
zhangyiming <yiming.zhang@sinosun.com.cn>
2022-06-01 13:44:47 +0800

    提交少量无关代码

--- KotlinStudy/app/src/main/java/com/study/algorithm/AlgotithmJava1.java
+++ KotlinStudy/app/src/main/java/com/study/algorithm/AlgotithmJava1.java
@@ -83,5 +83,39 @@ class AlgotithmJava1 {
      * 插入排序
      */
 
+    /**
+     * 两数之和
+     * 给一个整数数组，找到两个数使得他们的和等于一个给定的数 target。
+     * 你需要实现的函数twoSum需要返回这两个数的下标, 并且第一个下标小于第二个下标。注意这里下标的范围是 0 到 n-1。
+     * 你可以假设只有一组答案。
+     * 样例：
+     输入：
+     numbers = [2,7,11,15]
+     target = 9
+     输出：
+     [0,1]
+     解释：
+     numbers[0] + numbers[1] = 9
+     * 样例：
+     输入：
+     numbers = [15,2,7,11]
+     target = 9
+     输出：
+     [1,2]
+     解释：
+     numbers[1] + numbers[2] = 9
+     */
+    public static class  Solution3 {
+        public static int[] twoSum(int[] numbers, int target) {
+            int[] result = new int[2];
+            if (numbers.length < 2) {
+                result[0] = -1;
+                result[1] = -1;
+                return result;
+            }
+        }
+    }
+
+
 
 }
